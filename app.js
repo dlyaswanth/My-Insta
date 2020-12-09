46ff5013b1213399f9a47240548192ed14d85dbd
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2020-12-09 10:54:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 app.use(require('./routes/user'))
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('frontend/build'))
+    app.use(express.static('client/build'))
         const path=require('path')
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(_dirname,'frontend','build','index.html'))
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
         })
         }
 app.listen(PORT,()=>{
